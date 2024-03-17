6a2668c8c8ccac6c2d72b949ff6fb88a9fe5ce1e
darkjaffs <hurairajaffs2003@gmail.com>
2024-03-18 01:27:54 +0500

    Add the functionality to change color on mouseover

--- script.js
+++ script.js
@@ -1,25 +1,40 @@
 
-document.addEventListener('DOMContentLoaded', function () {
+const container = document.getElementById('container');
+generateGrid();
 
-    const container = document.getElementById('container');
+function generateGrid() {
+    document.addEventListener('DOMContentLoaded', function () {
 
-    let rows = 16;
-    let columns = 16;
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < columns; j++) {
-            const div = document.createElement('div');
+        let rows = 16;
+        let columns = 16;
 
-            div.classList.add("grid-item");
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < columns; j++) {
+                const div = document.createElement('div');
 
-            // Set the position of each grid item
-            div.style.left = j * 100 + "px";
-            div.style.top = i * 100 + "px";
+                div.classList.add("grid-item");
 
-            // Append the grid item to the body
-            container.appendChild(div);
+                // Set the position of each grid item
 
+                div.textContent = '';
+                div.style.width = '50px';
+                div.style.height = '50px';
+                // Append the grid item to the body
+                container.appendChild(div);
+
+                div.addEventListener("mouseover", () => {
+                    div.style.backgroundColor = 'black';
+                })
+
+            }
         }
-    }
-});
+    });
+}
+
+
+
+
+
+
 
