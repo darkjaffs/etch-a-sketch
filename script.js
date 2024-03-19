6ef2ad56cf6c816e02828661adc14592924bea0c
darkjaffs <hurairajaffs2003@gmail.com>
2024-03-19 18:59:25 +0500

    Add dynamic grid size allocation

--- script.js
+++ script.js
@@ -5,21 +5,22 @@ generateGrid();
 function generateGrid() {
     document.addEventListener('DOMContentLoaded', function () {
 
-
-        let rows = 16;
-        let columns = 16;
+        
+        let rows = 10;
+        let columns = 10;
+        let gridSize = 500/rows;
 
         for (let i = 0; i < rows; i++) {
             for (let j = 0; j < columns; j++) {
                 const div = document.createElement('div');
 
                 div.classList.add("grid-item");
-
+               
                 // Set the position of each grid item
-
-                div.textContent = '';
-                div.style.width = '50px';
-                div.style.height = '50px';
+                div.style.display = 'flex';
+                div.style.width = `${gridSize}px`;
+                div.style.height = `${gridSize}px`;
+                div.style.flex = '1 1 auto';
                 // Append the grid item to the body
                 container.appendChild(div);
 
