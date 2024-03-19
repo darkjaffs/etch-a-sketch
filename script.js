390128ebcbe77974971ac10975256a55373ae8dd
darkjaffs <hurairajaffs2003@gmail.com>
2024-03-19 21:16:18 +0500

    Add functionality to reset grid

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
 
 const container = document.getElementById('container');
-generateGrid();
+const resetbtn = document.getElementById('reset');
+
+initializeGrid();
 
 function generateGrid() {
-    document.addEventListener('DOMContentLoaded', function () {
 
-        
-        let rows = 10;
-        let columns = 10;
-        let gridSize = 500/rows;
+    let gridElement = 16;
+    let gridSize = 500 / gridElement;
 
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < columns; j++) {
-                const div = document.createElement('div');
+    for (let i = 0; i < gridElement; i++) {
+        for (let j = 0; j < gridElement; j++) {
+            const div = document.createElement('div');
 
-                div.classList.add("grid-item");
-               
-                // Set the position of each grid item
-                div.style.display = 'flex';
-                div.style.width = `${gridSize}px`;
-                div.style.height = `${gridSize}px`;
-                div.style.flex = '1 1 auto';
-                // Append the grid item to the body
-                container.appendChild(div);
+            div.classList.add("grid-item");
 
-                div.addEventListener("mouseover", () => {
-                    div.style.backgroundColor = 'black';
+            // Set the position of each grid item
+
+            div.style.width = `${gridSize}px`;
+            div.style.height = `${gridSize}px`;
+
+            // Append the grid item to the body
+            container.appendChild(div);
+
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = 'black';
+            })
+
+            resetbtn.addEventListener('click', function () {
+
+                const gridItems = document.querySelectorAll('.grid-item');
+                let gridArray = Array.from(gridItems);
+
+                gridItems.forEach(item => {
+                    item.style.backgroundColor = '';
                 })
 
-            }
+                
+            });
+
         }
-    });
+    }
+};
+
+function initializeGrid() {
+    document.addEventListener('DOMContentLoaded', generateGrid());
 }
+
+
+
+
+
+
+
+
 
 
 
